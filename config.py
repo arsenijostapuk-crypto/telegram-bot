19eeb778abe591aafef0e5442069e6d294506d9b
arsenijostapuk-crypto <arsenijostapuk@gmail.com>
2025-12-21 17:31:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # Конфігурація
-ADMIN_IDS = [5587016153, 1224217528]  
+ADMIN_IDS = [5587016153, 1224217528, 6165680123]  
 ADMIN_GROUP_ID= -1003654920245  # група для спілкування
 
 # Функції перевірки
 def is_admin(user_id):
     return user_id in ADMIN_IDS
+
 
 
