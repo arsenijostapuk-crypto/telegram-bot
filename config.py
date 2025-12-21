b85bdbd62ebb3bad7ec4c83dc0418f47d38d99b2
arsenijostapuk-crypto <arsenijostapuk@gmail.com>
2025-12-21 15:44:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # Конфігурація
-ADMIN_IDS = [5587016153]  # ваш Telegram ID
+ADMIN_IDS = [5587016153, 1224217528]  
 ADMIN_GROUP_ID= -1003654920245  # група для спілкування
 
 # Функції перевірки
 def is_admin(user_id):
     return user_id in ADMIN_IDS
+
 
